4d65f580366d725b5081e6d0968000f5d2fecf1d
satya kavya <satyakavya24@gmail.com>
2020-10-21 10:18:56 -0500

    edit reservation

--- reservation.js
+++ reservation.js
@@ -1,9 +1,9 @@
 $("#submit-btn").on("click", function(event){
     const newReservation = {
         name: $("#name").val(),
-        phoneNum: $("#phone-number").val()
-        // email: $("#email").val()
-        // id: $("#id").val()
+        phoneNum: $("#phone-number").val(),
+         email: $("#email").val(),
+        id: $("#id").val()
 
     };
 })
