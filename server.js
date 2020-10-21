af5ef9fb23e9fca834cd2d23724ca59b141bdfe3
satya kavya <satyakavya24@gmail.com>
2020-10-21 14:53:40 -0500

    add string

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(public));
+app.use(express.static("public"));
 
 var reservations = [];
 
