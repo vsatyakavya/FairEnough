0053dbeb91424164ab5e5526b9be2084fb624cd2
Cristina Ruiz <cristinaruiz96@yahoo.com>
2020-10-21 10:29:26 -0500

    Made the HTML pages more uniform

---
+++ reservation.js
@@ -0,0 +1,12 @@
+$("#submit-btn").on("click", function(event){
+    const newReservation = {
+        name: $("#name").val(),
+        phoneNum: $("#phone-number").val()
+        // email: $("#email").val()
+        // id: $("#id").val()
+
+    };
+})
+
+module.exports = Reservation
+
