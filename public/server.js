2d8bc9674d46b9ffb2588c566d078eab9cfb42fa
satya kavya <satyakavya24@gmail.com>
2020-10-21 11:50:37 -0500

    edit port

--- public/server.js
+++ public/server.js
@@ -4,7 +4,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 3011;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
